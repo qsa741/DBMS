b368afa2d1f8bc4064dcff3b96b89e5acada865a
yangjunyoung <junyoung.yang@myvtw.co.kr>
2021-11-30 14:36:50 +0900

    오타 수정

--- dbms/src/main/java/com/project/dbms/sql/DbmsSQL.java
+++ dbms/src/main/java/com/project/dbms/sql/DbmsSQL.java
@@ -35,10 +35,10 @@ public class DbmsSQL {
 	@Value("${spring.datasource.url}")
 	private String url;
 	
-	@Value("${spring.datasource.username")
+	@Value("${spring.datasource.username}")
 	private String username;
 	
-	@Value("${spring.datasource.password")
+	@Value("${spring.datasource.password}")
 	private String password;
 	
 	
